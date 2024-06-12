789bb365cbba666a79dc0d15c039f4f5f1282646
jessica-pereira-souza <126929259+jessica-pereira-souza@users.noreply.github.com>
2024-06-12 23:50:12 +0000

    12/06/24

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado: "Pergunta 1",
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper realistas. Qual o seu primeiro pensamento?",
     alternativas: [
-      "Resposta 1 ",
-      "Resposta 2"
+      "Isso é assustador.",
+      "Isso é maravilhoso."
     ]
   },
 
   {
-    enunciado: "Quem é o melhor vilão Jason ou Fred Krugger",
+    enunciado: "Com a descoberta desta tecnologia chamada Inteligência Artificial (IA), uma professora de tecnologia de uma escola decidiu fazer uma sequência de aulas sobre essa ferramenta. No fim de uma aula ela pede que você escreva um trabalho sobre o uso da IA em sala de aula. Qual atitude você toma?",
     alternativas: [
-      "Jason ",
-      "Fred"
+      "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique em uma linguagem que facilite o entendidmento.",
+      "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas que teve na internet e conhecimentos próprios sobre o tema."
     ]
   },
 
@@ -65,7 +65,25 @@ function mostraPergunta() {
 
 mostraPergunta();
 
+let atual = 0;
+let perguntaAtual;
 
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+
+mostraPergunta();
 
 
 
